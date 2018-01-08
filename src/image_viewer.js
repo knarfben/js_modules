6c4b4a1b7354ab0955921cdac98090240b836c83
knarfben <knarfben110@gmail.com>
2018-01-08 03:43:00 +0100

    managing images

--- src/image_viewer.js
+++ src/image_viewer.js
@@ -1,14 +1,11 @@
-import big from '../assets/big.jpg';
 import small from '../assets/small.jpg';
-
 import '../styles/image_viewer.css';
 
-const image = document.createElement('img');
-image.src = small;
+export default () => {
+    const image = document.createElement('img');
+    image.src = small;
+    
+    document.body.appendChild(image);
+};
 
-document.body.appendChild(image);
 
-const bigImage = document.createElement('img');
-bigImage.src = big;
-
-document.body.appendChild(bigImage);
